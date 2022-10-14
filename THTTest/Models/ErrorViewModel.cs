249f090758ebbaf6853ca83849038507d9357be5
Abdul Qadir <abdulqadir@technoheaven.net>
2022-10-14 15:51:09 +0400

    Add project files.

---
+++ THTTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace THTTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
